6fe4cbee936e18c22e15e16b613abc0d9f2d8c7c
eager7 <panchagntao@gmail.com>
2017-12-27 13:59:47 +0800

    1. add key and challenge buf

--- sha1/main.c
+++ sha1/main.c
@@ -18,87 +18,54 @@
 #include "sha1.h"  
 /* 
 * Define patterns for testing 
-*/  
-//#define TEST1 "abc"  
-char TEST1[] = {00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,0x01,0x23};
-#define TEST2a "abcdbcdecdefdefgefghfghighijhi"  
-  
-#define TEST2b "jkijkljklmklmnlmnomnopnopq"  
-#define TEST2 TEST2a TEST2b  
-#define TEST3 "a"  
-#define TEST4a "01234567012345670123456701234567"  
-#define TEST4b "01234567012345670123456701234567"  
-/* an exact multiple of 512 bits */  
-#define TEST4 TEST4a TEST4b  
-char *testarray[4] =  
-{  
-    TEST1,  
-    TEST2,  
-    TEST3,  
-    TEST4  
-};  
-  
-long int repeatcount[4] = { 1, 1, 1000000, 10 };  
-char *resultarray[4] =  
-{  
-    "A9 99 3E 36 47 06 81 6A BA 3E 25 71 78 50 C2 6C 9C D0 D8 9D",  
-    "84 98 3E 44 1C 3B D2 6E BA AE 4A A1 F9 51 29 E5 E5 46 70 F1",  
-    "34 AA 97 3C D4 C4 DA A4 F6 1E EB 2B DB AD 27 31 65 34 01 6F",  
-    "DE A3 56 A2 CD DD 90 C7 A7 EC ED C5 EB B5 63 93 4F 46 04 52"  
-};  
-  
+*/   
+#define PrintArray(x) do{printf("Name:%s, Length:%ld\n", #x, sizeof(x));for(int i = 0; i < sizeof(x);i++){\
+		printf("0x%02x ", x[i]);\
+	}printf("\n\n");}while(0)
+const char key[] = {00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00};
+const char challenge[] = {00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,0x01,0x23};
+char TEST[36] = {0};
+
 int main()  
 {  
     SHA1Context sha;  
     int i, j, err;  
     uint8_t Message_Digest[20];  
-    /* 
-    * Perform SHA-1 tests 
-    */  
-    //for(j = 0; j < 4; ++j)  
-    {  
-        //printf( "\nTest %d: %ld, ’%s’\n",j+1,repeatcount[j],testarray[j]);  
-        err = SHA1Reset(&sha);  
-        //if (err)  
-        //{  
-            //fprintf(stderr, "SHA1Reset Error %d.\n", err );  
-            //break; /* out of for j loop */  
-        //}  
-        //for(i = 0; i < repeatcount[j]; ++i)  
-        {  
-            err = SHA1Input(&sha,  
-                (const unsigned char *) TEST1,  
-                sizeof(TEST1));  
-            if (err)  
-            {  
-                fprintf(stderr, "SHA1Input Error %d.\n", err );  
-                //break; /* out of for i loop */  
-            }  
-        }  
-        err = SHA1Result(&sha, Message_Digest);  
-        if (err)  
-        {  
-            fprintf(stderr,  
-                "SHA1Result Error %d, could not compute message digest.\n",  
-                err );  
-        }  
-        else  
-        {  
-            printf("\t");  
-            for(i = 0; i < 20 ; ++i)  
-            {  
-                printf("%02X ", Message_Digest[i]);  
-            }  
-            printf("\n");  
-        }  
-        //printf("Should match:\n");  
-        //printf("\t%s\n", resultarray[j]);  
-    }  
+	
+	PrintArray(key);
+	PrintArray(challenge);
+	
+	err = SHA1Reset(&sha);  
+	if (err)  
+	{  
+		fprintf(stderr, "SHA1Reset Error %d.\n", err );  
+		return -1;
+	}  
+	memcpy(TEST, key, sizeof(key));
+	memcpy(&TEST[sizeof(key)], challenge, sizeof(challenge));
+	PrintArray(TEST);
+	err = SHA1Input(&sha, (const unsigned char *) TEST, sizeof(TEST));  
+	if (err)  
+	{  
+		fprintf(stderr, "SHA1Input Error %d.\n", err );  
+		return -1; 
+	}  
+	
+	err = SHA1Result(&sha, Message_Digest);  
+	if (err)  
+	{  
+		fprintf(stderr,"SHA1Result Error %d, could not compute message digest.\n", err );  
+		return -1;
+	}  
+	else  
+	{  
+		printf("SHA-1:\t");  
+		for(i = 0; i < 20 ; ++i)  
+		{  
+			printf("%02X ", Message_Digest[i]);  
+		}  
+		printf("\n");  
+	}    
   
-    /* Test some error returns */  
-    err = SHA1Input(&sha,(const unsigned char *) testarray[1], 1);  
-    printf ("\nError %d. Should be %d.\n", err, shaStateError );  
-    err = SHA1Reset(0);  
-    printf ("\nError %d. Should be %d.\n", err, shaNull );  
     return 0;  
 }  
